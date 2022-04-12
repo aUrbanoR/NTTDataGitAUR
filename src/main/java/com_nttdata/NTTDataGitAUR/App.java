8c13801cc5fb285ae6f6f74aa253edfee69d37ee
aUrbano24 <alfonso.urbano.fp@iescampanillas.com>
2022-04-12 18:30:10 +0200

    Fase Inicial

---
+++ src/main/java/com_nttdata/NTTDataGitAUR/App.java
@@ -0,0 +1,13 @@
+package com_nttdata.NTTDataGitAUR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola Mundo!" );
+    }
+}
